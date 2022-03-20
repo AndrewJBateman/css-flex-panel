661783583ad22e93fde8d7b8f75cbf876729626f
AndrewJBateman <gomezbateman@yahoo.com>
2022-03-20 17:25:50 +0100

    chore: linting, colors changed

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
-  this.classList.toggle('open'); 
+  this.classList.toggle("open");
 }
 
 function toggleActive(e) {
-  console.log('property name: ', e.propertyName); // returns flex-grow, font-size, transform
-  if(e.propertyName.includes('flex')) { // to work with all browsers, incl firefox
-     this.classList.toggle('open-active');
+  console.log("property name: ", e.propertyName); // returns flex-grow, font-size, transform
+  if (e.propertyName.includes("flex")) {
+    // to work with all browsers, incl firefox
+    this.classList.toggle("open-active");
   }
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
